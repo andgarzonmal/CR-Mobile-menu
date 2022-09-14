8d24c18d10a2c3cc234aa7d9623de1b2718706e1
andgarzonmal <andgarzonmal@unal.edu.co>
2022-09-14 09:46:00 -0500

    fix linters

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-menuBars = document.querySelector('.menu-bars')
-header = document.querySelector('.header')
-NavOpenMenu = document.querySelector('.nav-open')
-NavCloseMenu = document.querySelector('.close-open')
-
+const menuBars = document.querySelector('.menu-bars');
+const NavOpenMenu = document.querySelector('.nav-open');
+const NavCloseMenu = document.querySelector('.close-open');
 
 const openNav = () => {
-  NavOpenMenu.style.display ='flex'
-}
+  NavOpenMenu.style.display = 'flex';
+};
 
 const closeNav = () => {
-  NavOpenMenu.style.display ='none'
-}
+  NavOpenMenu.style.display = 'none';
+};
 
 menuBars.addEventListener('click', () => {
-  openNav()
-})
+  openNav();
+});
 
 NavCloseMenu.addEventListener('click', () => {
-  closeNav()
-})
+  closeNav();
+});
